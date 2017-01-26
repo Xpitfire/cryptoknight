bf7792af77024d28adb6daa01d913f71cfce632b
Marius-Constantin Dinu <dinu.marius-constantin@hotmail.com>
2017-01-26 21:07:41 +0100

    Added register command

--- src/CryptoKnight.Server.Console/Commands/RegisterUserCommand.cs
+++ src/CryptoKnight.Server.Console/Commands/RegisterUserCommand.cs
@@ -1,7 +1,5 @@
 ﻿using CryptoKnight.Server.KeyGenerator;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using CryptoKnight.Library.Network;
 using CryptoKnight.Server.Core;
 
@@ -10,18 +8,27 @@ namespace CryptoKnight.Server.Console.Commands
     class RegisterUserCommand : ICommand<Program>
     {
         public string Description => "Register User";
+        private const string CancellationCommand = "exit";
+        private static readonly string CommandInfo = $"Please type in a valid email or '{CancellationCommand}' to quit >";
 
         public void Execute(InputOutputHandler io, Program host)
         {
             string email = null;
-            while (string.IsNullOrEmpty(email = io.ReadString("Please type in the:"))) ; // wait for valid input
+            // wait for valid input
+            while (string.IsNullOrEmpty(email = io.ReadString(CommandInfo)))
+            {
+                if (string.Equals(email, CancellationCommand, StringComparison.InvariantCultureIgnoreCase)) return;
+                io.WriteLine(CommandInfo);
+            }
+
             var oneWayPassword = new Generator { Template = "kkkkkk" }.CreateKey();
             io.WriteLine($"Your one-way-password is: {oneWayPassword.Code}");
             host.LicenseService.RegisterUser(
-                new User {
-                Email = email,
-                PasswordHash = oneWayPassword.Code.ComputeMd5Hash()
-            });
+                new User
+                {
+                    Email = email,
+                    PasswordHash = oneWayPassword.Code.ComputeMd5Hash()
+                });
         }
     }
 }
--- src/CryptoKnight.Server.Core/DefaultLicenseServiceImpl.cs
+++ src/CryptoKnight.Server.Core/DefaultLicenseServiceImpl.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using CryptoKnight.Server.KeyGenerator;
 
@@ -6,6 +7,11 @@ namespace CryptoKnight.Server.Core
 {
     public class DefaultLicenseServiceImpl : ILicenseService
     {
+        public void RegisterUser(User user)
+        {
+            KeyStore.RegisterUser(user);
+        }
+
         public Key RequestLicenseKey(User user)
         {
             return KeyStore.RequestLicenseKey(user);
